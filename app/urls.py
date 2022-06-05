83a9c07e92511cc28d5957985d02867ffc87a034
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2022-06-05 20:27:33 +0100

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,25 @@
-from .views import AirCraftView
+from .views import (
+    AirCraftView,
+    AirPortView,
+    FlightView,
+    departure_flights,
+    departure_search,
+    flight_search,
+)
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
 
 
 urlpatterns = [
-    path('login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('refresh_token/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    path('aircraft/', AirCraftView.as_view(), name="aircraft"),
+    path("login/", jwt_views.TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("refresh_token/", jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
+    path("aircraft/", AirCraftView.as_view(), name="aircraft"),
+    path("aircraft/<str:uid>/", AirCraftView.as_view(), name="aircraft-dets"),
+    path("airport/", AirPortView.as_view(), name="airport"),
+    path("airport/<str:uid>/", AirPortView.as_view(), name="airport-dets"),
+    path("flight/", FlightView.as_view(), name="flight"),
+    path("flight/search/", flight_search, name="flight-search"),
+    path("flight/<str:uid>/", FlightView.as_view(), name="flight-dets"),
+    path("departures/search/", departure_search, name="departure-search"),
+    path("departures/flights/<str:uid>/", departure_flights, name="departure-flights"),
 ]
