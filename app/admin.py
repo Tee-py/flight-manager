7fe6f9522370a8e00774e71f99b6484675816f23
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2022-06-05 20:25:55 +0100

    register models

--- app/admin.py
+++ app/admin.py
@@ -1,2 +1,7 @@
-#from django.contrib import admin
+from django.contrib import admin
+from .models import Aircraft, Airport, Location, Flight
 
+admin.site.register(Aircraft)
+admin.site.register(Airport)
+admin.site.register(Location)
+admin.site.register(Flight)
