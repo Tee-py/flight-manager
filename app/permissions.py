b727ebf9ca414c5bc9fc72697a8ec22b95c33057
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2022-06-05 20:26:20 +0100

    Added permissions

--- app/permissions.py
+++ app/permissions.py
@@ -1,19 +1,15 @@
 from rest_framework.permissions import IsAuthenticated
 
-class IsAdmin(IsAuthenticated):
 
+class IsAdmin(IsAuthenticated):
     def has_permission(self, request, view):
         if not super().has_permission(request, view):
             return False
         return request.user.role == "admin"
 
-class IsUser(IsAuthenticated):
 
+class IsUser(IsAuthenticated):
     def has_permission(self, request, view):
         if not super().has_permission(request, view):
             return False
         return request.user.role == "user"
-
-        
-
-
